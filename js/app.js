0260336fed03c6f1649358a1d88c1322c3ac2a2d
Marioandre <50428621+marioandrepereira@users.noreply.github.com>
2023-03-20 23:06:01 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
+const loginForm = document.querySelector(".sign-in-form");
 
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
@@ -9,3 +10,18 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // evita que o formulário seja enviado
+
+  const username = loginForm.querySelector("input[type='text']").value.trim();
+  const password = loginForm.querySelector("input[type='password']").value.trim();
+
+  if (username && password) {
+    window.location.href = "index.html";
+  } else {
+    alert("Por favor, preencha ambos os campos de texto.");
+  }
+});
+
+
