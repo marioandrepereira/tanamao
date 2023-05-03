09bd8f82448027128ed2aeb0ad03ecc79560ad5b
marioandrepereira <you@example.com>
2023-05-03 19:22:00 -0300

    arrumado

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,12 +16,31 @@ loginForm.addEventListener("submit", (event) => {
 
   const username = loginForm.querySelector("input[type='text']").value.trim();
   const password = loginForm.querySelector("input[type='password']").value.trim();
+  const formContainer = loginForm.closest(".forms-container");
 
-  if (username && password) {
+  if (password) {
     window.location.href = "index.html";
   } else {
-    alert("Por favor, preencha ambos os campos de texto.");
+    formContainer.classList.add("shake");
+    setTimeout(() => {
+      formContainer.classList.remove("shake");
+    }, 300);
   }
 });
+function formatCPF(cpf) {
+  // remove qualquer caractere que não seja um número
+  cpf.value = cpf.value.replace(/\D/g, '');
+
+  // limita o número de dígitos ao número total de dígitos de um CPF
+  if (cpf.value.length > 11) {
+    cpf.value = cpf.value.slice(0, 11);
+  }
+
+  // adiciona os pontos e o traço conforme o usuário preenche
+  cpf.value = cpf.value.replace(/^(\d{3})(\d)/g, '$1.$2');
+  cpf.value = cpf.value.replace(/^(\d{3})\.(\d{3})(\d)/g, '$1.$2.$3');
+  cpf.value = cpf.value.replace(/\.(\d{3})(\d)/g, '.$1-$2');
+  return cpf;
+}
 
 
