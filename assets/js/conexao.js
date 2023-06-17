9626e969fe6bb3f4aae2357ba15dc7db2861759a
marioandrepereira <you@example.com>
2023-06-17 18:40:56 -0300

    dell

--- assets/js/conexao.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-// Configurações da conexão com o banco de dados
-const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'tanamao',
-  password: '150908mario',
-  port: 5432, // Porta padrão do PostgreSQL é 5432
-});
